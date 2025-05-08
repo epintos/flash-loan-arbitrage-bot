d94db64cc854bcfc53ca9d8c4557b4e1d84e9541
Esteban Pintos <epintos7@gmail.com>
2025-05-08 17:53:46 +0200

    Implements getDEXPrice and makes dex configuration more generic

--- smart-contracts/src/FlashLoanArbitrage.sol
+++ smart-contracts/src/FlashLoanArbitrage.sol
@@ -2,40 +2,35 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { IVault } from "@balancer-labs/v2-interfaces/vault/IVault.sol";
 import { IFlashLoanRecipient } from "@balancer-labs/v2-interfaces/vault/IFlashLoanRecipient.sol";
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { IERC20 } from "@balancer-labs/v2-interfaces/solidity-utils/openzeppelin/IERC20.sol";
+import { IUniswapV2Router02 } from "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import { IUniswapV2Factory } from "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+import { IUniswapV2Pair } from "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 
 contract FlashLoanArbitrage is IFlashLoanRecipient, Ownable {
     error FlashLoanArbitrage__NotBalancerVault();
     error FlashLoanArbitrage__NotEnoughToRepayFlashLoan();
+    error FlashLoanArbitrage__InvalidAmountOfRoutersAndFactories();
 
     // Address of the Balancer Vault for flash loans
     address private balancerVault;
 
-    // Router addresses for different DEXes
-    address private uniswapRouter;
-    address private sushiswapRouter;
+    // Router addresses for different Uniswap V2 Fork DEXes
+    address[] private dexRouters;
 
-    // Factory addresses for different DEXes
-    address private uniswapFactory;
-    address private sushiswapFactory;
+    // Factory addresses for different Uniswap V2 Fork DEXes
+    address[] private dexFactories;
 
-    constructor(
-        address _balancerVault,
-        address _uniswapRouter,
-        address _uniswapFactory,
-        address _sushiswapRouter,
-        address _sushiswapFactory
-    )
-        Ownable(msg.sender)
-    {
+    constructor(address _balancerVault, address _dexRouters, address _dexFactories) Ownable(msg.sender) {
         balancerVault = _balancerVault;
-        uniswapRouter = _uniswapRouter;
-        uniswapFactory = _uniswapFactory;
-        sushiswapRouter = _sushiswapRouter;
-        sushiswapFactory = _sushiswapFactory;
+        if (_dexRouters.length != _dexFactories.length && _dexRouters.length != 2) {
+            revert FlashLoanArbitrage__InvalidAmountOfRoutersAndFactories();
+        }
+        dexRouters = _dexRouters;
+        dexFactories = _dexFactories;
     }
 
     /**
@@ -104,5 +99,53 @@ contract FlashLoanArbitrage is IFlashLoanRecipient, Ownable {
 
         // Approve the Balancer Vault to pull the tokens back
         tokens[0].approve(balancerVault, amountToRepay);
+    }
+
+    /**
+     * @dev Gets the pair price from a Uniswap V2 fork DEX
+     * @param tokenIn Address of the input token
+     * @param tokenOut Address of the output token
+     * @param amountIn Amount of input tokens
+     * @return amountOut Amount of output tokens
+     */
+    function getDEXPrice(
+        uint256 dexIndex,
+        address tokenIn,
+        address tokenOut,
+        uint256 amountIn
+    )
+        internal
+        view
+        returns (uint256 amountOut)
+    {
+        IUniswapV2Factory factory = IUniswapV2Factory(dexFactories[dexIndex]);
+        IUniswapV2Pair pair = IUniswapV2Pair(factory.getPair(tokenIn, tokenOut));
+
+        // Liquidity pool does not exist
+        if (address(pair) == address(0)) {
+            return 0;
+        }
+
+        // Gets Liquidity pool reserves
+        (uint256 reserve0, uint256 reserve1,) = pair.getReserves();
+
+        uint256 reserveIn;
+        uint256 reserveOut;
+
+        // Reserves are ordered by token address
+        // This means that the token with the lower address is reserve0
+        if (tokenIn < tokenOut) {
+            reserveIn = reserve0;
+            reserveOut = reserve1;
+        } else {
+            reserveIn = reserve1;
+            reserveOut = reserve0;
+        }
+
+        // Calculate the price using the constant product formula
+        // 0.3% fee: https://docs.uniswap.org/contracts/v2/concepts/advanced-topics/fees
+        // Proudct formula: x * y = k => (tokenAReserve + amountIn * 0.997) * (tokenB - amountOut) = k
+        // https://docs.uniswap.org/contracts/v2/concepts/protocol-overview/glossary#constant-product-formula
+        amountOut = (amountIn * 997 * reserveOut) / ((reserveIn * 1000) + (amountIn * 997));
     }
 }
